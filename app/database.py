9cc3874b33f4211fbe03b415a2e764ba4a4ff2db
Anubhab Dey <anubhabdey2017@gmail.com>
2025-07-19 22:17:25 +0530

    [Database][ database.py ][1:23][Initial commit of database.py][This file manages the connection to the database and gives you the connection object.]

---
+++ app/database.py
@@ -0,0 +1,22 @@
+# This file manages the DB Connectivity and gives you a session object when `get_db` is called.
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Session factory
+AsyncSessionLocal = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+# Dependency to get a session
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
