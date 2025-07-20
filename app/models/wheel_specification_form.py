e84824023fb64e5f0a5c2cf873a90c8ce3e8fb79
Anubhab Dey <anubhabdey2017@gmail.com>
2025-07-20 07:56:07 +0530

    [Database and Environment][{app/models/wheel_specification_form.py -- 1:20}{bogie_checksheet.py}][Created the schema wheel_specification_form.py and deleted all bogie_checksheet.py files][Created the file which defines the DB Schema of the table to store wheel specification data, and removed files pertaining to bogie checksheet because the two APIs that I have chosen aar the POST and the GET of Wheel Specification Form]

---
+++ app/models/wheel_specification_form.py
@@ -0,0 +1,20 @@
+# Contains code to declare the DB table schema to store wheel specifications
+
+
+
+from sqlalchemy import Column, String, Date, JSON
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+Base = declarative_base()
+
+class WheelSpecificationForm(Base):
+    __tablename__ = "wheel_specification_forms"
+
+    form_number = Column(String, primary_key=True, index=True)
+    submitted_by = Column(String, nullable=False)
+    submitted_date = Column(Date, nullable=False)
+    
+    # PostgreSQL native JSONB column for all the `fields` object
+    fields = Column(JSONB, nullable=False)
